f75eb544478278106631c77bbcb639a1ba45961f
Charles Simon <charles.cjsimon@gmail.com>
2022-11-20 12:53:16 -0700

    second draft added brandons cluster function

--- RecomEngine2.py
+++ RecomEngine2.py
@@ -59,6 +59,7 @@ class Engine:
         self.df1= self.df1[self.df1['imdbId'].isin(self.df2['imdb_id'])]
         self.df2 = self.df2.drop_duplicates(subset = 'imdb_id')
         self.df_cluster =  self.df1.copy()
+        self.df_filter =self.df1.copy()
         self.selection =0
     def set_K(self, x):
         self.K = x
@@ -102,72 +103,60 @@ class Engine:
               # Add movie selection
               self.selection = movie_list['imdbId'].iloc[index]
     def apply_clustering(self):
-         conditional = self.cluster_param
-         working_df = self.df1
-         selection = self.selection
-         clusters = self.K
-         if(conditional==0):
-             split_df = pd.DataFrame(working_df['genres'].tolist())
-             genres_object = split_df.stack()
-             genres_df = pd.get_dummies(genres_object)
-             genres_df = genres_df.groupby(level=0).sum()
-             R = genres_df.values
-         elif(conditional==1):
-             documents = []
-             #df_cluster = df_cluster.head(1000)
-             working_df['Title_No_Year'].map(lambda x: documents.append(str(x)))
-             vectorizer = TfidfVectorizer()
-             R = vectorizer.fit_transform(documents)
-         elif(conditional == 2):
-             documents = []
-             #df_cluster = df_cluster.head(1000)
-             working_df['Title_No_Year'].map(lambda x: documents.append(str(x)))
-             vectorizer = TfidfVectorizer()
-             X = vectorizer.fit_transform(documents)
-             km = KMeans(n_clusters=clusters, random_state=0)
-             km.fit(X)
-             labels = km.labels_
+        conditional = self.cluster_param
+        working_df = self.df1
+        selection = self.selection
+        clusters = self.K
+
+        tempcluster = pd.DataFrame()
+        model = KMeans(n_clusters=clusters, init='k-means++', n_init=1)
+
+        if conditional == 0 or conditional == 2:
+           genres = pd.get_dummies(working_df['genres'].explode(), prefix='G').groupby(level=0).sum()
+           tempcluster = genres
+
+        if conditional == 1 or conditional == 2:
+           titles = pd.DataFrame()
+           vectorizer = TfidfVectorizer()
+           X = vectorizer.fit_transform(working_df['Title_No_Year'])
+           model.fit(X)
+
+           titles.loc[:, 'Cluster'] = model.labels_
+           tempcluster['Cluster'] = model.labels_
+        if(conditional ==2 or conditional == 0):
+            model.fit(tempcluster)
+            working_df.loc[:, 'Cluster'] = model.labels_
+        if(conditional==1):
+            working_df.loc[:, 'Cluster'] = model.labels_
+        selection_cluster = int(working_df[working_df['imdbId']==self.selection].Cluster)
+        print('selection cluster', selection_cluster)
+        working_df = working_df.loc[working_df['Cluster'] == selection_cluster]
+        self.df_cluster = working_df
+        print(clusters, conditional, selection)
              
-             #normalize
-             labels = labels/max(labels)
-             
-             split_df = pd.DataFrame(working_df['genres'].tolist())
-             genres_object = split_df.stack()
-             genres_df = pd.get_dummies(genres_object)
-             genres_df = genres_df.groupby(level=0).sum()
-             genres_df['Titles']= labels
-             
-             R = genres_df.values
-             
-         kmeans = KMeans(n_clusters=clusters, random_state=0)
-         kmeans.fit(R)
-         labels = kmeans.labels_
-         self.df_cluster['cluster'] = labels
-         selection_cluster = self.df_cluster[self.df_cluster['imdbId']==self.selection].cluster
-         self.df_cluster=self.df_cluster[self.df_cluster['cluster'].isin(selection_cluster)]
-         print(clusters, conditional, selection)
+        
     def calculate_lev(self):
         base_case = self.df_cluster[(self.df_cluster['imdbId']==self.selection)]
         comparison_type = "Title_No_Year"
         base_case =base_case[comparison_type].to_list()
         print('lev distance', Levenshtein.distance(base_case[0], 'Toy_Story'))
         #print(base_case[comparison_type].astype(str))
-        self.df_cluster['levenshtein'] = self.df_cluster[comparison_type].map(lambda x: Levenshtein.distance(base_case[0], str(x)))
-        self.df_cluster['levenshtein'] =  1.0-(self.df_cluster['levenshtein'].astype(float)/float(max( self.df_cluster['levenshtein'])))
+        self.df_filter['levenshtein'] = self.df_filter[comparison_type].map(lambda x: Levenshtein.distance(base_case[0], str(x)))
+        self.df_filter['levenshtein'] =  1.0-(self.df_filter['levenshtein'].astype(float)/float(max( self.df_filter['levenshtein'])))
    
     def euclidean_distance(self, base_case_year: int, comparator_year: int):
         return abs(base_case_year - comparator_year)
     
     def calculate_euclid(self):
-        base_case = self.df_cluster[(self.df_cluster['imdbId']==self.selection)]
+        base_case = self.df_filter[(self.df_filter['imdbId']==self.selection)]
         comparison_type = "Year"
         #print(base_case[comparison_type])
-        self.df_cluster['euclidean'] = self.df_cluster[comparison_type].map(lambda x: self.euclidean_distance(int(base_case[comparison_type]), x))
+        self.df_filter['euclidean'] = self.df_filter[comparison_type].map(lambda x: self.euclidean_distance(int(base_case[comparison_type]), x))
         #print(max(self.df_cluster['euclidean']))
         #smalllest year is 0 for no year
-        without_zero_years = self.df_cluster[self.df_cluster[comparison_type]!=0]
+        without_zero_years = self.df_filter[self.df_filter[comparison_type]!=0]
         max_euclid= max(without_zero_years['euclidean'])
-        self.df_cluster['euclidean'] = 1.0-(self.df_cluster['euclidean'].astype(float)/max_euclid)    
+        self.df_filter['euclidean'] = 1.0-(self.df_filter['euclidean'].astype(float)/max_euclid)    
     def calculate_cosine(self):
         # list to store all the plots
          plots = []
@@ -191,20 +180,22 @@ class Engine:
 
          results = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
          results = np.transpose(results)
-         self.df_cluster['cosine_similarity'] = results[1:,:]
+         self.df_filter['cosine_similarity'] = results[1:,:]
     
     def filter_and_sort(self):
-        self.df_cluster['score'] = 0
+        self.df_filter =self.df_cluster.copy()
+        self.df_filter['score'] = 0
         if self.filter_title == True:
             self.calculate_lev()
-            self.df_cluster['score'] += self.title_weight*self.df_cluster['levenshtein']
+            self.df_filter['score'] += self.title_weight*self.df_filter['levenshtein']
         if self.filter_plot == True:
             self.calculate_cosine()
-            self.df_cluster['score'] += self.plot_weight*self.df_cluster['cosine_similarity']
+            self.df_filter['score'] += self.plot_weight*self.df_filter['cosine_similarity']
         if self.filter_year == True:
             self.calculate_euclid()
-            self.df_cluster['score'] +=self.a_year_weight* self.df_cluster['euclidean']
-        recommendations_df = self.df_cluster = self.df_cluster.sort_values(by = 'score', ascending=False)
+            self.df_filter['score'] +=self.a_year_weight* self.df_filter['euclidean']
+        recommendations_df = self.df_filter = self.df_filter.sort_values(by = 'score', ascending=False)
+        recommendations_df = recommendations_df[(recommendations_df['imdbId']!=self.selection)]
         return recommendations_df
         
         
